c79b7432844a0e2c44f053da6875873983995375
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-08-20 17:43:33 +0100

    feat: :sparkles: handle redirection and route protection

--- actions/create-account/index.ts
+++ actions/create-account/index.ts
@@ -22,6 +22,7 @@ export default async function ACreateAccount(signUpData: {
         phone,
         email,
         user_role: role || "user",
+        isComplete: true,
       },
     },
   });
@@ -47,6 +48,7 @@ export async function AUpdateAccount(signUpData: {
       phone,
       email,
       user_role: role || "user",
+      isComplete: true,
     },
   });
   if (error) {
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -38,33 +38,52 @@ export async function updateSession(request: NextRequest) {
     }
   );
 
-  // refreshing the auth token
   const { data: userData } = await supabase.auth.getUser();
-  if (request.nextUrl.pathname.startsWith("/auth") && userData?.user) {
-    const url = request.nextUrl.clone();
-    if (userData.user.user_metadata.user_role === 'manager'){
-      url.pathname = "/dashboard";
-    } else {
-      url.pathname = "/profile";
-    }
-    return NextResponse.redirect(url);
-  }
-  else if (
-    request.nextUrl.pathname.startsWith("/dashboard")
-  ) {
-    console.log("dashboard: ", userData);
-    if (userData && userData.user) {
-      if (userData.user.user_metadata.user_role === 'manager') {
-        return NextResponse.next();
-      } else {
-        const url = request.nextUrl.clone();
-        url.pathname = "/";
-        return NextResponse.redirect(url);
-      }
-    } else {
+
+  // handle user not logged in
+  if (!userData.user) {
+    if (
+      request.nextUrl.pathname.startsWith("/dashboard") ||
+      request.nextUrl.pathname.startsWith("/profile")
+    ) {
       const url = request.nextUrl.clone();
       url.pathname = "/auth";
       return NextResponse.redirect(url);
+    }
+  }
+  // handle user logged in
+  if (userData.user) {
+    console.log("User data: ", userData.user.user_metadata);
+    if (!userData?.user?.user_metadata.isComplete) {
+      // profile not complete and user try to access dashboard or profile
+      if (
+        request.nextUrl.pathname.startsWith("/dashboard") ||
+        request.nextUrl.pathname.startsWith("/profile") ||
+        request.nextUrl.pathname.startsWith("/auth") ||
+        (request.nextUrl.pathname.startsWith("/create-account") &&
+          !request.nextUrl.searchParams.get("code"))
+      ) {
+        const url = request.nextUrl.clone();
+        url.pathname = "/create-account";
+        url.searchParams.set("code", "complete-profile");
+        return NextResponse.redirect(url);
+      }
+    } else {
+      // profile complete and user try to access create account
+      if (request.nextUrl.pathname.startsWith("/create-account")) {
+        const url = request.nextUrl.clone();
+        url.pathname = "/dashboard";
+        return NextResponse.redirect(url);
+      } else if (request.nextUrl.pathname.startsWith("/auth")) {
+        // profile complete and user try to access auth page
+        const url = request.nextUrl.clone();
+        if (userData.user.user_metadata.user_role === "manager") {
+          url.pathname = "/dashboard";
+        } else {
+          url.pathname = "/profile";
+        }
+        return NextResponse.redirect(url);
+      }
     }
   }
 
