1b77efb737c965384ab9534d306e3c8a6ee30814
Mohammed Benkhattab <mobenkhattab@gmail.com>
2024-09-01 15:36:02 +0100

    feat: :sparkles: update user dropdown

--- components/protected/user-dropdown/user-dropdown.tsx
+++ components/protected/user-dropdown/user-dropdown.tsx
@@ -9,19 +9,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MdCardMembership } from "react-icons/md";
-import { useUser } from "@/app/context/UserContext";
 import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
 import { UserData } from "@/lib/getUserData";
+import Link from "next/link";
 
-export default function UserDropdownMenu({userData}: {userData: UserData}) {
+export default function UserDropdownMenu({ userData }: { userData: UserData }) {
   const router = useRouter();
 
   const handleSignOut = async () => {
     const supabase = createClient();
     await supabase.auth.signOut();
     router.push("/");
+    router.refresh();
   };
 
   return (
@@ -31,26 +31,28 @@ export default function UserDropdownMenu({userData}: {userData: UserData}) {
           <AvatarImage src={userData?.avatar_url!} alt="@shadcn" />
           <AvatarFallback className="font-bold text-violet-400">
             {`${userData?.first_name![0]}${
-                userData?.last_name![0]
+              userData?.last_name![0]
             }`.toUpperCase()}
           </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-44">
-        <DropdownMenuLabel>{userData?.first_name} {userData?.last_name}</DropdownMenuLabel>
+        <DropdownMenuLabel>
+          {userData?.first_name} {userData?.last_name}
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem>
-            <LucideClipboardList className="mr-2 h-6 w-4" />
-            <span>Appointmentes</span>
+            <Link className="flex items-center" href="/profile">
+              <LucideClipboardList className="mr-2 h-6 w-4" />
+              <span>Profile</span>
+            </Link>
           </DropdownMenuItem>
           <DropdownMenuItem>
-            <MdCardMembership className="mr-2 h-6 w-4" />
-            <span>Memberships</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings className="mr-2 h-6 w-4" />
-            <span>Settings</span>
+            <Link className="flex items-center" href="/settings">
+              <Settings className="mr-2 h-6 w-4" />
+              <span>Settings</span>
+            </Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
